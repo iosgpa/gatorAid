0499fac25dcb72496a9e616e37955a3ec5095e83
Kinderley Charles <kinderleycharles@aol.com>
2015-10-26 11:23:12 -0400

    Initial push. Parse framework added

---
+++ Gator Aid/Gator Aid/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gator Aid
+//
+//  Created by Kinderley Charles on 10/26/15.
+//  Copyright © 2015 Kinderley Charles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
